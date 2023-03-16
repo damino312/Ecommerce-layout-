444adb63ce478e204a5eb35a82fddf3964be8d77
damino312 <93064817+damino312@users.noreply.github.com>
2023-03-16 14:22:37 +0700

    init project
    prepare gulp and some html

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var rigger = require('gulp-rigger');
+const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const { watch, series } = require('gulp');
+
+function buildStyles() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+};
+
+
+
+function htmlImport() {
+  return gulp.src("./src/pages/**/*.html")
+    .pipe(rigger())
+    .pipe(gulp.dest('./public/pages'));
+}
+
+
+exports.buildStyles = buildStyles;
+exports.watcher = function () { // следит за изменениями в папке sass  
+  watch('./src/scss/**/*.scss',{ ignoreInitial: false },buildStyles); //выполняет при изменении
+  watch('./src/pages/**/*.html',{ ignoreInitial: false }, htmlImport);//выполняет при изменении
+};
